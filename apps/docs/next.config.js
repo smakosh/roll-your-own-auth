9645036c536c88a163f0b08685bd203c9ca0d12d
smakosh <ismai23l@hotmail.com>
2025-03-24 00:47:37 +0100

    chore: test export output

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
+  themeConfig: './theme.config.jsx',
+  output: "export"
 })
  
 module.exports = withNextra()
