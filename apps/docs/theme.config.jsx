56ad103ee5b969a0bafd63c0d5d7869f1b38b2a0
smakosh <ismai23l@hotmail.com>
2025-03-28 11:29:45 +0100

    fix: missing import in theme config

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,33 +1,48 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 
 export default {
   head: () => {
-    const { asPath, defaultLocale, locale } = useRouter()
-    const { frontMatter } = useConfig()
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
     const url =
-      'https://roll-your-own-auth.smakosh.com' +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+      "https://roll-your-own-auth.smakosh.com" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
     return (
-    <>
-      <meta property="og:url" content={url} />
-      <meta property='og:image' content="https://roll-your-own-auth.smakosh.com/og-image.png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content={frontMatter.title || "Roll Your Own Auth"} />
-      <meta property="og:description" content={frontMatter.description || "Skip the risks of third-party auth. Get started with a simple, secure, and self-hosted authentication system"} />
-    </>
-  )},
+      <>
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:image"
+          content="https://roll-your-own-auth.smakosh.com/og-image.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "Roll Your Own Auth"}
+        />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "Skip the risks of third-party auth. Get started with a simple, secure, and self-hosted authentication system"
+          }
+        />
+      </>
+    );
+  },
   logo: <span>Roll your own Auth</span>,
   project: {
-    link: 'https://github.com/smakosh/roll-your-own-auth'
+    link: "https://github.com/smakosh/roll-your-own-auth",
   },
-  docsRepositoryBase: 'https://github.com/smakosh/roll-your-own-auth/tree/main/apps/docs',
+  docsRepositoryBase:
+    "https://github.com/smakosh/roll-your-own-auth/tree/main/apps/docs",
   useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
       return {
-        titleTemplate: '%s – Ryo-auth'
-      }
+        titleTemplate: "%s – Ryo-auth",
+      };
     }
   },
-}
+};
